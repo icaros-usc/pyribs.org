d479779ac1248b6cd133d3700e51e0a1291456a2
Bryon Tjanaka <bryon@btjanaka.net>
2021-02-14 22:07:52 -0800

    Switch colors to extend rather than override; tweak gray-100

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 // Configuration for tailwind.css
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   purge: [
     "./src/**/*.html",
@@ -37,26 +35,21 @@ module.exports = {
           },
         },
       },
-    },
-    colors: {
-      primary: {
-        dark: "#603f8c",
-        semidark: "#6f4ba7",
-        DEFAULT: "#7e57c2",
-        light: "#f1eafd",
+      colors: {
+        primary: {
+          dark: "#603f8c",
+          semidark: "#6f4ba7",
+          DEFAULT: "#7e57c2",
+          light: "#f1eafd",
+        },
+        secondary: {
+          DEFAULT: "#e040fb",
+        },
+        light: "rgba(0,0,0,0.54)",
+        gray: {
+          100: "#f3f4f7",
+        },
       },
-      secondary: {
-        DEFAULT: "#e040fb",
-      },
-      light: "rgba(0,0,0,0.54)",
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
     },
   },
   variants: {
