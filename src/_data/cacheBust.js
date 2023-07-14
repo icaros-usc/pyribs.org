5d5c4ea488cfc8b5a4e722c70103554832376cde
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2023-07-13 23:23:20 -0700

    Handle scenarios where vendor.js does not exist

--- src/_data/cacheBust.js
+++ src/_data/cacheBust.js
@@ -1,12 +1,18 @@
 const md5File = require("md5-file");
+const fs = require("fs");
 
 const cacheBust = () => {
   // A "map" of files to cache bust.
   const files = {
     mainCss: "./src/_compiled-assets/main.css",
     mainJs: "./src/_compiled-assets/main.js",
-    vendorJs: "./src/_compiled-assets/vendor.js",
   };
+
+  // vendor.js is not always created.
+  const vendorJsPath = "./src/_compiled-assets/vendor.js";
+  if (fs.existsSync(vendorJsPath)) {
+    cacheBust.vendorJs = vendorJsPath;
+  }
 
   // In dev, add a md5 hash to the URL, and in production, add the date.
   return Object.entries(files).reduce((acc, [key, path]) => {
--- src/_webpack/js/index.jsx
+++ src/_webpack/js/index.jsx
@@ -1,5 +1,2 @@
 // CSS imports (necessary because Webpack uses single entry point).
 import "../css/index.scss";
-
-// Placeholder until we get real code that needs vendor.js.
-Array.from(document.getElementsByTagName("p")).forEach((p) => {});
